808654deb81a618892035b6eb71a2ff5e33619cc
Sarawut Pe <mr.sarawutpe@gmail.com>
2024-01-14 08:27:48 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/Home.tsx';
+import './index.css';
+import 'material-icons/iconfont/material-icons.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
